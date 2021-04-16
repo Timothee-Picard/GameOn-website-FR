c0928afe350a91e51aeb205f4604cb214bde0434
Timothee-Picard <timothee@atnos.com>
2021-04-16 18:04:43 +0200

    fix: repair html

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,14 +10,21 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const modalClose = document.querySelectorAll(".close");
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+modalClose.forEach((btnclose) => btnclose.addEventListener("click", closeModal));
+
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+function closeModal() {
+  modalbg.style.display = "none";
 }
 
 
